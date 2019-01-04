c73ec6662929f919bc97c4a7741784ebb46b0aee
lemefe21 <lemefe21@gmail.com>
2019-01-04 12:44:51 -0200

    S03, T05a and T05b solved

--- ud851-Sunshine/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ ud851-Sunshine/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -29,6 +29,6 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    // OK (3) Create a menu with an item with id of action_share
+    // OK (4) Display the menu and implement the forecast sharing functionality
 }
--- ud851-Sunshine/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ ud851-Sunshine/S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -221,7 +221,7 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
             return true;
         }
 
-        // TODO (2) Launch the map when the map menu item is clicked
+        // OK (2) Launch the map when the map menu item is clicked
 
         return super.onOptionsItemSelected(item);
     }
